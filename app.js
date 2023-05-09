533002936d7c1315005d453faa9e95b0cf02454f
Felipe Fazani <56308348+felipefazani@users.noreply.github.com>
2023-05-09 09:37:48 -0300

    add ejs

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require('express');
-const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const sessionsRouter = require('./src/routers/sessionsRouter');
+// const sessionsRouter = require('./src/routers/sessionsRouter');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
+// app.set('views', './src/views');
+// app.set('view engine', 'ejs');
 
-app.use('/sessions', sessionsRouter);
+// app.use('/sessions', sessionsRouter);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
 });
 
 app.listen(PORT, () => {
-  debug(`listening on port ${chalk.green(PORT)}`);
+  console.log(`listening on port ${PORT}`);
 });
