9855489371b91d3d336ceb3edb8c69a051a0a71f
Felipe Fazani <56308348+felipefazani@users.noreply.github.com>
2023-05-09 09:44:44 -0300

    add npm start

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const debug = require('debug')('app');
+// const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
@@ -10,7 +10,7 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
-// app.set('views', './src/views');
+app.set('views', './src/views');
 // app.set('view engine', 'ejs');
 
 // app.use('/sessions', sessionsRouter);
