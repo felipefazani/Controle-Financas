a0a0f2f57e4d0470e833de49a58a10646b45baa4
= <=>
2023-05-19 20:10:39 -0300

    create azure mysql database

--- app.js
+++ app.js
@@ -2,18 +2,40 @@ const express = require('express');
 // const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const fs = require('fs');
+const mysql = require('mysql');
+const { database, server, user, password } = require('./serverconfig.js');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-// const sessionsRouter = require('./src/routers/sessionsRouter');
+
+const config =
+{
+    host: `${server}.mysql.database.azure.com`,
+    user: `${user}`,
+    password: `${password}`,
+    database: `${database}`,
+    port: 3306,
+    ssl: {ca: fs.readFileSync("./DigiCertGlobalRootCA.crt.pem")}
+};
+
+const conn = new mysql.createConnection(config);
+conn.connect(
+  function (err) {
+      if (err) {
+          console.log("!!! Cannot connect !!! Error:");
+          throw err;
+      }
+      else {
+          console.log("Connection established.");
+      }
+  });
+
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
 app.set('views', './src/views');
-// app.set('view engine', 'ejs');
-
-// app.use('/sessions', sessionsRouter);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
