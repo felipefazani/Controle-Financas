a3a8163e6b08a6bff9f8d37a42ab2d7c4587fa66
Nathan Fonseca <104946940+NeitF@users.noreply.github.com>
2023-06-02 09:11:58 -0300

    reset merge

--- app.js
+++ app.js
@@ -1,104 +1,25 @@
 const express = require('express');
-const session = require('express-session');
-// const debug = require('debug')('app');
+const chalk = require('chalk');
+const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const bcrypt = require("bcrypt");
-const saltRound = 10;
-const cookieParser = require('cookie-parser');
-const conn = require("./src/config/databaseconnection");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const sessionsRouter = require('./src/routers/sessionsRouter');
 
-//routing
-const indexRouter = require('./src/routers/index.js');
-const authRouter = require('./src/routers/authRouter.js');
-const homeRouter = require('./src/routers/homeRouter.js');
-
-//setting up 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use(cookieParser());
-app.use(session({secret: 'top10financas'}));
-
-//passport
-require('./src/config/passport.js')(app)
-  
-app.post("/query", (req, res) => {
-  conn.query(req.body.query, (err, result) => { res.send(result) })
-});
-
-app.post("/register", (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email; 
-  const pswd = req.body.password;
-
-  bcrypt.hash(pswd, saltRound, (err, hash) => {
-    conn.query(
-      `SELECT * FROM user WHERE email = '${email}'`,
-      (err, result) => {
-        if (err) {
-          res.send(err);
-        }
-        if (result.length == 0) {
-          conn.query(
-            `INSERT INTO user values (NULL, '${name}', '${email}', '${hash}')`,
-            (err, result) => {
-              if (err){
-                res.send(err);
-              }
-              res.send({ msg: "Registered completed"});
-            });
-        } else {
-          res.send({ msg: "This user is already registered"});
-        }
-      }
-    );
-  });
-});
-
-app.post("/login", (req, res) => {
-  const pswd = req.body.password;
-  const email = req.body.email;
-
-  conn.query(
-    `SELECT * FROM user WHERE email='${req.body.email}'`,
-    (err, result) => {
-      if (err)
-        res.send(err);
-      
-      if (result.length == 0){
-        res.send({msg: "email incorrect"});
-      }else{
-        bcrypt.compare(pswd, result[0].password, 
-          (erro, result) =>{
-            if (result) {
-              res.send({msg: "user logged"});
-            } else {
-              res.send({msg: "password incorrect"});
-            }
-          }
-        );
-        
-      }
-    }
-  );
-});
-
 
 app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.use('/home', homeRouter);
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
+app.use('/sessions', sessionsRouter);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
 });
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  debug(`listening on port ${chalk.green(PORT)}`);
 });
