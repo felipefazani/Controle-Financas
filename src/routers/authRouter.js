0735f176e95e8db56988da4abd7eb44b36844930
= <=>
2023-06-03 11:10:28 -0300

    password and confirm password verification

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -11,39 +11,43 @@ authRouter.route('/signUp').post((req, res) => {
   const email = req.body.email;
   const pswd = req.body.password;
 
-  // TODO create verification password and confirmPassword
-
-  bcrypt.hash(pswd, saltRound, (err, hash) => {
-    conn.query(
-      `SELECT * FROM user WHERE email = '${email}'`,
-      (err, result) => {
-        if (err) {
-          res.send("Error in signup: \n" + err);
-        }
-
-        if (result.length == 0) {
-          conn.query(
-            `INSERT INTO user values (NULL, '${name}', '${email}', '${hash}')`,
-            (err, result) => {
-              if (err) {
-                res.send(err);
-              }
-              user = {
-                username: req.body.username,
-                email: req.body.email,
-              }
-              req.login(user, () => {
-                console.log({ msg: "Registered completed" });
-                res.redirect('/home');
+  if (pswd != req.body.confirmPassword) {
+    res.send({msg : "Password and Confirm Password doesn't coincide "});
+    // Error("Incorrect password and confirm password");
+  } else {  
+    bcrypt.hash(pswd, saltRound, (err, hash) => {
+      conn.query(
+        `SELECT * FROM user WHERE email = '${email}'`,
+        (err, result) => {
+          if (err) {
+            res.send("Error in signup: \n" + err);
+          }
+  
+          if (result.length == 0) {
+            conn.query(
+              `INSERT INTO user values (NULL, '${name}', '${email}', '${hash}')`,
+              (err, result) => {
+                if (err) {
+                  res.send(err);
+                }
+                user = {
+                  username: req.body.username,
+                  email: req.body.email,
+                }
+                req.login(user, () => {
+                  console.log({ msg: "Registered completed" });
+                  res.redirect('/home');
+                });
               });
-            });
-
-        } else {
-          res.send({ msg: "This user is already registered" });
+  
+          } else {
+            res.send({ msg: "This user is already registered" });
+          }
         }
-      }
-    );
-  });
+      );
+    });
+  }
+
 });
 
 authRouter
