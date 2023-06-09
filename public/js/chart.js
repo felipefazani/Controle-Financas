001f2e9ea923ad51b2ab5dd67afd5cbb8b977467
Nathan Fonseca <nafs2002@hotmail.com>
2023-06-09 18:02:03 -0300

    criando tela de relatorios

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,50 +1,102 @@
 const ctx = document.getElementById('myChart');
+const ctx2 = document.getElementById('chartRelatorio');
 
-  new Chart(ctx, {
-    type: 'doughnut',
-    data: {
+new Chart(ctx, {
+  type: 'doughnut',
+  data: {
     //   labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 0,
-        backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-      }]
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 0,
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+    }]
+  },
+  options: {
+    scales: {
+      x: {
+        beginAtZero: true,
+        ticks: {
+          display: false
+        },
+        grid: {
+          display: false,
+          drawTicks: false
+        },
+        border: {
+          display: false
+        }
+      },
+      y: {
+        beginAtZero: true,
+        ticks: {
+          display: false
+        },
+        grid: {
+          display: false,
+          drawTicks: false
+        },
+        border: {
+          display: false
+        }
+      },
+
     },
-    options: {
-      scales: {
-        x: {
-            beginAtZero: true,
-            ticks: {
-              display: false
-            },
-            grid: {
-              display: false,
-              drawTicks: false
-            },
-            border: {
-              display: false
-            }
+  },
+
+});
+
+new Chart(ctx2, {
+  type: 'bar',
+  data: {
+    labels: ['Receita', 'Despesa'],
+    datasets: [{
+      label: '',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 0,
+      backgroundColor: [
+        '#1D9000',
+        '#F12020'
+      ],
+    },]
+  },
+  options: {
+    scales: {
+      x: {
+        beginAtZero: true,
+        ticks: {
+          // display: false
         },
-        y: {
-          beginAtZero: true,
-          ticks: {
-            display: false
-          },
-          grid: {
-            display: false,
-            drawTicks: false
-          },
-          border: {
-            display: false
-          }
+        grid: {
+          display: false,
+          drawTicks: false
         },
-        
+        border: {
+          display: false
+        }
+      },
+      y: {
+        beginAtZero: true,
+        ticks: {
+          display: false
+        },
+        grid: {
+          display: false,
+          drawTicks: false
+        },
+        border: {
+          display: false
+        }
       },
     },
-   
-  });
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  }
+})
