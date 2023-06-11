d8b5a914bec88a2eadbd3dad0a8d7cacb19fe6bd
Nathan Fonseca <nafs2002@hotmail.com>
2023-06-11 20:09:30 -0300

    Finalizadno gráfico e tabela da pg Relatórios

--- public/js/chart.js
+++ public/js/chart.js
@@ -50,26 +50,124 @@ new Chart(ctx, {
 
 });
 
-new Chart(ctx2, {
+// new Chart(ctx2, {
+//   type: 'bar',
+//   data: {
+//     labels: ['Receita', 'Despesa'],
+//     datasets: [{
+//       label: '',
+//       data: [12, 19, 3, 5, 2, 3],
+//       borderWidth: 0,
+//       backgroundColor: [
+//         '#1D9000',
+//         '#F12020'
+//       ],
+//     },]
+//   },
+//   options: {
+//     scales: {
+//       x: {
+//         beginAtZero: true,
+//         ticks: {
+//           // display: false
+//         },
+//         grid: {
+//           display: false,
+//           drawTicks: false
+//         },
+//         border: {
+//           display: false
+//         }
+//       },
+//       y: {
+//         beginAtZero: true,
+//         ticks: {
+//           display: false
+//         },
+//         grid: {
+//           display: false,
+//           drawTicks: false
+//         },
+//         border: {
+//           display: false
+//         }
+//       },
+//     },
+//     plugins: {
+//       legend: {
+//         display: false
+//       }
+//     }
+//   }
+// })
+
+
+const data = {
+  labels: ['01/01', '02/01', '03/01', '04/01', '05/01', '06/01',
+  '07/01', '08/01', '09/01', '10/01', '11/01', '12/01', '13/01',
+  '14/01', '15/01', '16/01', '17/01', '18/01', '19/01', '20/01',
+  '21/01', '22/01', '23/01', '24/01', '25/01', '26/01', '27/01',
+  '28/01', '29/01', '30/01',],
+  datasets: [{
+    label: 'Receita',
+    data: [18, 12, 6, 9, 12, 3, 9, 18, 12, 6, 9, 12, 3, 9, 18, 12, 6, 9, 12, 3, 9,
+      18, 12, 6, 9, 12, 3, 9, 3, 9, ],
+    backgroundColor: 'rgba(54, 162, 235, 1)',
+    borderColor: 'rgba(54, 162, 235, 1)',
+    order:2,
+    datalabels: {
+      color: 'white',
+      font: {
+        weight: 'bold'
+      }
+    }
+  },{
+    label: 'Despesa',
+    data: [18, 12, 6, 9, 12, 3, 9, 18, 12, 6, 9, 12, 3, 9, 18, 12, 6, 9, 12, 3, 9,
+      18, 12, 6, 9, 12, 3, 9, 3, 9, ],
+    backgroundColor: 'rgba(255, 26, 104, 1)',
+    borderColor: 'rgba(255, 26, 104, 1)',
+    order: 2,
+    datalabels: {
+      color: 'rgba(255, 26, 104, 1)',
+      anchor: 'end',
+      align: 'end',
+      offset: 5,
+      font: {
+        weight: 'bold'
+      }
+    }
+  }
+  // ,{
+  //   label: 'Green Line',
+  //   data: [18, 12, 6, 9, 12, 3, 9],
+  //   backgroundColor: 'rgba(75, 192, 192, 1)',
+  //   borderColor: 'rgba(75, 192, 192, 1)',
+  //   type: 'line',
+  //   order: 1,
+  //   datalabels: {
+  //    display: false
+  // }
+  // }
+]
+};
+
+// config 
+const config = {
   type: 'bar',
-  data: {
-    labels: ['Receita', 'Despesa'],
-    datasets: [{
-      label: '',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 0,
-      backgroundColor: [
-        '#1D9000',
-        '#F12020'
-      ],
-    },]
-  },
+  data,
   options: {
     scales: {
       x: {
+        stacked: true,
         beginAtZero: true,
         ticks: {
           // display: false
+          callback: (value, index, values) => {
+            if((index % 2) === 0){
+              return data.labels[index]
+            }
+          }
         },
         grid: {
           display: false,
@@ -80,6 +178,8 @@ new Chart(ctx2, {
         }
       },
       y: {
+        grace: '5%',
+        stacked: true,
         beginAtZero: true,
         ticks: {
           display: false
@@ -95,8 +195,17 @@ new Chart(ctx2, {
     },
     plugins: {
       legend: {
-        display: false
+        // display: false
       }
     }
-  }
-})
+  },
+  plugins: [ChartDataLabels]
+};
+
+// render init block
+const chartRelatorios = new Chart(
+  document.getElementById('chartRelatorio'),
+  config
+);
+
+
