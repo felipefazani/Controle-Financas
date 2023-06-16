9e510f572020554e9b40321d0aea256ce906bc17
Flávio Barbosa <flavio.sbarbosa@telefonica.com>
2023-06-15 21:30:17 -0300

    insert transactions update

--- src/api/accounts.js
+++ src/api/accounts.js
@@ -66,7 +66,7 @@ async function getAccount(idUser) {
     if (idUser === undefined) {
       reject({ msg: "idUser is null, try again!", error: true, code: 1000 });
       return;
-    } 
+    }
     const query = `SELECT * FROM account WHERE id_user = '${idUser}'`;
     const result = await databaseQuery(query);
 
@@ -79,6 +79,85 @@ async function getAccount(idUser) {
         msg = "This user doesn't exists in database!";
       } else {
         msg = "This user has no accounts in database!";
+      }
+
+      reject({ msg, error: true, code: 1001 });
+      return;
+    } else {
+      resolve(result);
+      return;
+    }
+  });
+}
+
+async function insertTransactions(date, value, type, description, idAccount, idCategory) {
+  return new Promise(async (resolve, reject) => {
+    let varName = false;
+
+    if (idCategory === undefined)
+      varName = "idCategory";
+    if (value === undefined)
+      varName = "value";
+    if (date === undefined)
+      varName = "date";
+    if (description === undefined)
+      varName = "description";
+    if (type === undefined)
+      varName = "type";
+    if (idAccount === undefined)
+      varName = "idAccount";
+
+    if (varName) {
+      reject({ msg: `${varName} is null, try again!`, error: true, code: 1000 });
+      return;
+    }
+    else {
+      const insertTransactionsQuery = `INSERT INTO transactions ( date, description, value, type, id_account, id_category)
+                                  VALUES ('${date}', '${description}', '${value}', '${type}', '${idAccount}', '${idCategory}')`;
+      const insertTransactionsResult = await databaseQuery(insertTransactionsQuery);
+
+      if(type){
+        const updateAccountQuery = `UPDATE account SET current_balance= current_balance + ${value} WHERE id_account = ${idAccount}`;
+        const updateBillResult = await databaseQuery(updateAccountQuery);
+      }else{
+        const updateAccountQuery = `UPDATE account SET current_balance= current_balance - ${value} WHERE id_account = ${idAccount}`;
+        const updateBillResult = await databaseQuery(updateAccountQuery);
+      }
+
+      transactionsAc = {
+        idTransactions: insertTransactionsResult.insertId,
+        date,
+        value,
+        type,
+        description,
+        idAccount,
+        idCategory
+      }
+      resolve(transactionsAc);
+      return;
+    }
+  }
+  )
+};
+
+async function getTransactions(idAccount) {
+  return new Promise(async (resolve, reject) => {
+    if (idAccount === undefined) {
+      reject({ msg: "idAccount is null, try again!", error: true, code: 1000 });
+      return;
+    }
+    const query = `SELECT * FROM transactions WHERE id_account = '${idAccount}'`;
+    const result = await databaseQuery(query);
+
+    if (result.length == 0) {
+      const userQuery = `SELECT * FROM account WHERE id_account = '${idAccount}'`;
+      const userResult = await databaseQuery(userQuery);
+
+      let msg;
+      if (userResult.length == 0) {
+        msg = "This account doesn't exists in database!";
+      } else {
+        msg = "This account has no transactions in database!";
       }
 
       reject({ msg, error: true, code: 1001 });
@@ -111,6 +190,32 @@ accountRouter.route('/getAccount').post(async (req, res) => {
 
   try {
     const result = await getAccount(idUser);
+    res.send(result);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+accountRouter.route('/insertTransactions').post(async (req, res) => {
+  const date = req.body.date;
+  const type = req.body.type;
+  const value = req.body.value;
+  const description = req.body.description;
+  const idAccount = req.body.idAccount;
+  const idCategory = req.body.idCategory;
+  if (idAccount === undefined)
+    res.send({ msg: "idAccount is null, try again!", error: true, code: 1000 });
+  else {
+    const result = await insertTransactions(date, value, type, description, idAccount, idCategory);
+    res.send(result);
+  }
+});
+
+accountRouter.route('/getTransactions').post(async (req, res) => {
+  const idAccount = req.body.idAccount;
+
+  try {
+    const result = await getTransactions(idAccount);
     res.send(result);
   } catch (error) {
     res.send(error);
