a8e3586d1e0ddef2f2a3e75eab8a6efa802591aa
= <=>
2023-11-01 14:36:46 -0300

    create getTransactions

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,77 +1,95 @@
 async function getUserProfile() {
-    await fetch("/auth/profile", { method: "get" })
-      .then(response => response.text())
-      .then(data => {
-        user = JSON.parse(data);
-      })
-  
-    return user;
+  await fetch("/auth/profile", { method: "get" })
+    .then(response => response.text())
+    .then(data => {
+      user = JSON.parse(data);
+    })
+
+  return user;
+}
+
+async function createUserAccount(userId) {
+  options = {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      idUser: userId,
+      bank: "standard",
+      description: "first account",
+      currentBalance: 0
+    })
   }
-  
-  async function createUserAccount(userId) {
-    options = {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        idUser: userId,
-        bank: "standard",
-        description: "first account",
-        currentBalance: 0
-      })
-    }
-  
-    await fetch("/api/accounts/insertAccount", options)
-      .then(response => response.text())
-      .then(data => {
-        account = JSON.parse(data);
-      })
-  
-    return account;
+
+  await fetch("/api/accounts/insertAccount", options)
+    .then(response => response.text())
+    .then(data => {
+      account = JSON.parse(data);
+    })
+
+  return account;
+}
+
+async function getUserAccounts(userId) {
+  options = {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      idUser: userId
+    })
   }
-  
-  async function getUserAccounts(userId) {
-    options = {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        idUser: userId
-      })
+
+  await fetch("/api/accounts/getAccount", options)
+    .then(response => response.text())
+    .then(data => {
+      accounts = JSON.parse(data);
+    })
+
+  if (accounts.error) {
+    if (accounts.msg == "This user has no accounts in database!") {
+      return await createUserAccount(userId);
     }
-  
-    await fetch("/api/accounts/getAccount", options)
-      .then(response => response.text())
-      .then(data => {
-        accounts = JSON.parse(data);
-      })
-  
-    if (accounts.error) {
-      if (accounts.msg == "This user has no accounts in database!") {
-        return await createUserAccount(userId);
-      }
-    } else {
-      return accounts[0];
-    }
+  } else {
+    return accounts[0];
   }
-  
-  async function insertTransactions(isInput, date, value, description, idAccount, idCategory) {
-    options = {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        date: date,
-        type: isInput,
-        value: value,
-        description: description,
-        idAccount: idAccount,
-        idCategory: idCategory,
-      })
-    }
-  
-    await fetch("/api/accounts/insertTransactions", options)
-      .then(response => response.text())
-      .then(data => {
-        transaction = JSON.parse(data);
-      })
-  
-    return transaction;
+}
+
+async function insertTransactions(isInput, date, value, description, idAccount, idCategory) {
+  options = {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      date: date,
+      type: isInput,
+      value: value,
+      description: description,
+      idAccount: idAccount,
+      idCategory: idCategory,
+    })
   }
+
+  await fetch("/api/accounts/insertTransactions", options)
+    .then(response => response.text())
+    .then(data => {
+      transaction = JSON.parse(data);
+    })
+
+  return transaction;
+}
+
+async function getTransactions(idAccount) {
+  options = {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      idAccount: idAccount,
+    })
+  }
+
+  await fetch("/api/accounts/getTransactions", options)
+    .then(response => response.text())
+    .then(data => {
+      transactions = JSON.parse(data);
+    })
+
+  return transactions;
+}
