9ea512ad2db6955e43f9f89f6c5c2394d86b18e0
Nathan Fonseca <nathan.silva@becomex.com.br>
2023-11-01 08:10:15 -0300

    remove the last database config

--- src/config/databaseconnection.js
+++ src/config/databaseconnection.js
@@ -4,18 +4,6 @@ const { database, server, user, password } = require('./serverconfig');
 const path = require('path');
 
 
-//database config
-const config =
-{
-    host: `${server}.mysql.database.azure.com`,
-    user: `${user}`,
-    password: `${password}`,
-    database: `${database}`,
-    port: 3306,
-    ssl: {ca: fs.readFileSync(path.join(__dirname, './DigiCertGlobalRootCA.crt.pem'))}
-};
-
-
 const conn = new mysql.createConnection(database);
 conn.connect(
   function (err) {
